4d6b3e76ca593f1e8b51cfa663a28d705efc3f89
AndresCaiza31 <andres.caiza@epn.edu.ec>
2026-01-18 03:27:45 -0500

    Organización: Proyecto movido a carpeta contenedora springboot_app

---
+++ springboot_app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO estudiante (nombre, email, edad, carrera) VALUES ('Andrés Caiza', 'andres@ejemplo.com', 20, 'Software');
+INSERT INTO estudiante (nombre, email, edad, carrera) VALUES ('Ana García', 'ana@ejemplo.com', 22, 'Sistemas');
+INSERT INTO estudiante (nombre, email, edad, carrera) VALUES ('Carlos Pérez', 'carlos@ejemplo.com', 19, 'Computación');
+INSERT INTO estudiante (nombre, email, edad, carrera) VALUES ('Lucía Fernández', 'lucia@ejemplo.com', 21, 'Software');
+INSERT INTO estudiante (nombre, email, edad, carrera) VALUES ('Roberto Gómez', 'roberto@ejemplo.com', 23, 'Sistemas');
