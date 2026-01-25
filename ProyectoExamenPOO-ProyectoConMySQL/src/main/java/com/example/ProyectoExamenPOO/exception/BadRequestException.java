7cf4df7abdbe4b586c4fee27f0b5079b9a0407b2
Adonis_Lopez <adonis.lopez@epn.edu.ec>
2026-01-25 16:45:07 -0500

    Se agrega funcion de ordenamiento

--- ProyectoExamenPOO-ProyectoConMySQL/src/main/java/com/example/ProyectoExamenPOO/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProyectoExamenPOO.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- ProyectoExamenPOO-ProyectoConMySQL/src/main/java/com/example/ProyectoExamenPOO/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProyectoExamenPOO.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
